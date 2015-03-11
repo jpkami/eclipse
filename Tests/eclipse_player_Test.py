28f352aa5f4f6d961c4a2fd39c7e58675081285c
Euqaidoz <charles.delort@gmail.com>
2015-03-11 19:40:21 +0100

    added DefineWinner in Game and computeScore in player

--- Tests/eclipse_player_Test.py
+++ Tests/eclipse_player_Test.py
@@ -12,7 +12,7 @@ class Test(unittest.TestCase):
 
 
     def setUp(self):
-        pass
+        self.joueur = Human()
 
 
     def tearDown(self):
@@ -22,21 +22,24 @@ class Test(unittest.TestCase):
     def test_player_setup(self):
         
         joueur = HegemonieOrion()
-        print(joueur.flotte[0].type)
-        print("joueur.maxmove " +str(joueur.maxMove))
-        print("joueur.tuilescombat " +str(joueur.tuilesCombat))
-        print("joueur.maxreputcombat " +str(joueur.maxReputationCombat))
-        print("joueur.science " +str(joueur.science))
+#         print(joueur.flotte[0].type)
+#         print("joueur.maxmove " +str(joueur.maxMove))
+#         print("joueur.tuilescombat " +str(joueur.tuilesCombat))
+#         print("joueur.maxreputcombat " +str(joueur.maxReputationCombat))
+#         print("joueur.science " +str(joueur.science))
         
         self.assertTrue(joueur.flotte[0].type == "Fregate")
         
         joueur = Human()
-        print(joueur.flotte[0].type)
-        print(joueur.flotte[0])
-        print(joueur.flotte[0].prix)
+#         print(joueur.flotte[0].type)
+#         print(joueur.flotte[0])
+#         print(joueur.flotte[0].prix)
         
         self.assertTrue(joueur.flotte[0].type == "Intercepteur")
 
+    def test_computeScore(self):
+        print(self.joueur.computeScore())
+        self.assertTrue(self.joueur.computeScore() == 0)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
